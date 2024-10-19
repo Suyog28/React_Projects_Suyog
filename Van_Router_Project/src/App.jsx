2f14562622ecff750b76e1d3d6e7d3e2e7a8b055
Suyog28 <suyogvmuley@gmail.com>
2024-10-19 12:37:15 +0530

    Boostrap of routing

--- Van_Router_Project/src/App.jsx
+++ Van_Router_Project/src/App.jsx
@@ -3,21 +3,30 @@ import './App.css'
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans/Vans'
+import VanDetails from "./pages/Vans/VanDetails"
+import "./server"
+import Layout from './components/Layout'
+import Dashboard from './pages/Host/Dashboard'
+import Invoice from './pages/Host/Invoice'
+import Reviews from './pages/Host/Reviews'
 
 function App() {
 
 
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetails />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/invoice" element={<Invoice />} />
+          <Route path="/host/reviews" element={<Reviews />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
