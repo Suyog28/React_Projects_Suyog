3ca115778748e7862bb4a34959545800e0cd8f0a
Suyog28 <suyogvmuley@gmail.com>
2024-10-22 20:51:13 +0530

    Vanlife Project Completed

--- Van_Router_Project/src/components/Layout.jsx
+++ Van_Router_Project/src/components/Layout.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './Header'
+import React from "react"
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
+import Footer from "./Footer"
 
-function Layout() {
+export default function Layout() {
     return (
-
-        <>
+        <div className="site-wrapper">
             <Header />
-            <Outlet />
-        </>
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     )
 }
-
-export default Layout
